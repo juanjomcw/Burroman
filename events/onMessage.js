768d1708d7a4bd0723dbf51ca85e1d8d3b321f4c
juanjomcw <37007743+juanjomcw@users.noreply.github.com>
2020-12-21 00:47:55 -0700

    Update onMessage.js

--- events/onMessage.js
+++ events/onMessage.js
@@ -93,6 +93,8 @@ module.exports = (message, client) => {
                 return sendStream(message);
             case 'peeporuga':
                 return sendPeeporuga(message);
+            case 'user':
+                return sendUser(message);
             default:
                 break;
         }
@@ -524,4 +526,113 @@ const sendStream = (message) => {
 const sendPeeporuga = (message) => {
     message.delete()
     message.channel.send('<:whp1:786677945682755597><:pepePoint:761701490766643260>')
+}
+
+const sendUser = (message) => {
+    
+    let tmpMessage;
+    if (!message.member.roles.cache.find(r => r.name === "Admin")) {
+            tmpMessage = await message.channel.send(`Perdon <@${message.author.id}>, pero no tienes el permiso para usar el comando <:sadKEK:761281532035596349>`);
+            return tmpMessage.delete({ timeout: 5000 });
+        }
+
+    let estados = {
+        "online": "🟢 En Línea",
+        "idle": "🌙 Ausente",
+        "dnd": "🔴 No molestar",
+        "offline": "⚪ Desconectado/invisible"
+      };
+
+      const member = message.mentions.members.first() || message.member
+
+      function formatDate (template, date) {
+        var specs = 'YYYY:MM:DD:HH:mm:ss'.split(':')
+        date = new Date(date || Date.now() - new Date().getTimezoneOffset() * 6e4)
+        return date.toISOString().split(/[-:.TZ]/).reduce(function (template, item, i) {
+          return template.split(specs[i]).join(item)
+        }, template)
+      }
+
+
+      let badges1 = {
+     
+        'DISCORD_PARTNER': '<:partner:790483011610869831> ',
+        'HOUSE_BRAVERY': '<:bravery:790483010696511517>',
+        'HOUSE_BRILLIANCE': '<:Brilliance:790483011056566313>',
+        'HOUSE_BALANCE': '<:BALANCE:790483010394128404>',
+
+
+       
+      }
+
+      let obj = {
+        "HOUSE_BRAVERY" : "Bravery" , "HOUSE_BRILLIANCE" : "Brilliance" , "DISCORD_PARTNER" : "Socio de discord"
+        }
+
+
+        const embed = new MessageEmbed() 
+        .setDescription("**INFORMACIÓN DEL USUARIO:**")
+        .setColor(`#ff8000`)
+        .addFields(
+            {
+               name:"**🎫 Nombre**:",
+               value: "**" + `${member.user.tag}` + "**",
+               inline: true
+            },
+            {
+                name:"**🎟 ID**:",
+               value: `${member.user.id}`,
+               inline: true
+            },
+            {
+                name: "**📌 Apodo del usuario**:",
+                value: message.member.nickname ? message.member.nickname : "No tiene apodo", 
+                inline: true 
+            },
+            {
+                name:"**🛎 Fecha de Ingreso al Servidor:**",
+                value: formatDate('DD/MM/YYYY, a las HH:mm:ss', member.joinedAt),
+                inline: true
+
+            },
+            {
+                name: "**📥 Cuenta Creada:**",
+                value: formatDate('DD/MM/YYYY, a las HH:mm:ss', member.user.createdAt),
+                inline: true
+
+            },
+            {
+                name:"**🏳️ Insignias:**",
+                value: member.user.flags.toArray().length ? member.user.flags.toArray().map(badge => badges1[badge]).join(' ') : "No tiene insignias",
+                inline: true
+
+            },
+            {
+                name: "**🎨 Estado**:",
+                value: member.user.presence.activities[0] ? member.user.presence.activities[0].state : "Sin estado",
+                inline: true// En linea: SI
+            },
+            {
+                name: "**🎖 Roles**:",
+                value: member.roles.cache.map(roles => `\`${roles.name}\``).join(', '),
+                inline: true
+
+            },
+            {
+                name: "**🌐 Actividad**:",
+                value: "" + estados[member.user.presence.status] + "",
+                inline: true
+
+            },
+            {
+                name:"**🚀 ¿Boostea?**:",
+                value: member.premiumSince ? '**Estoy boosteando '+message.guild.name+' <:boost:790483016509030411> **' : '**No Boosteo '+message.guild.name+' <:SadCat:790465642066083852> **',
+                inline: true
+
+            }
+        )
+            .setThumbnail (member.user.displayAvatarURL({ format: "png", dynamic: true, size: 1024 }))
+            .setFooter(`${message.author.username}`, `${message.author.displayAvatarURL({dynamic: true})}`)
+            message.channel.send(embed)
+
 }
