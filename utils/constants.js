2e5a1dc0896959f2ebb04e15f08117c2ccb984d3
juanjomcw <37007743+juanjomcw@users.noreply.github.com>
2020-12-26 11:11:58 -0700

    Update constants.js

--- utils/constants.js
+++ utils/constants.js
@@ -1,5 +1,5 @@
 let constants = {
-    on_ready_broadcast_channel_id: '784548314506330183', //Canal log
+    on_ready_broadcast_channel_id: '763513335789518848', //Canal log
 
     server_id: '732788013264011355', //ID server Varelandia
     reglas_id: '779033867637620798', //ID canal de reglas
