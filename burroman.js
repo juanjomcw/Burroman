86e332a7c867d374347b072b84d32176b3dd2128
juanjomcw <37007743+juanjomcw@users.noreply.github.com>
2021-01-05 15:19:50 -0700

    Update burroman.js

--- burroman.js
+++ burroman.js
@@ -4,15 +4,11 @@ const client = new Discord.Client();
 
 /* Events */
 const onReady = require('./events/onReady');
-const onGuildMemberAdd = require('./events/onGuildMemberAdd');
 const onMessage = require('./events/onMessage');
-const onGuildMemberRemove = require('./events/onGuildMemberRemove');
 
 
 
 client.on("ready", () => onReady(client));
-client.on('guildMemberAdd', onGuildMemberAdd);
-client.on('guildMemberRemove', onGuildMemberRemove);
 client.on('message', (message) => onMessage(message, client));
 
 
